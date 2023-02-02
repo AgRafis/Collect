9b236cb04ce3d089c8166170fa00db195dd18ebd
Rafis <Rafis@gmail.com>
2023-02-02 19:38:02 +0500

    array list done

--- User.java
+++ User.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class User {
+    int id;
+    String name;
+    int age;
+    ArrayList<String> phoneNumbers;
+
+    public void fillingFields(int id, String name, int age, ArrayList<String> phoneNumbers) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phoneNumbers = phoneNumbers;
+
+    }
 
 }
