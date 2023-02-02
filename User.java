653a2b06b04f727b46b24d3fcbaabac1ea569c87
Rafis <Rafis@gmail.com>
2023-02-02 23:17:01 +0500

    array list edit

--- User.java
+++ User.java
@@ -6,12 +6,10 @@ public class User {
     int age;
     ArrayList<String> phoneNumbers;
 
-    public void fillingFields(int id, String name, int age, ArrayList<String> phoneNumbers) {
+    public User(int id, String name, int age, ArrayList<String> phoneNumbers) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.phoneNumbers = phoneNumbers;
-
     }
-
 }
--- main.java
+++ main.java
@@ -4,29 +4,21 @@ public class main {
     public static void main(String[] args) {
         ArrayList<String> phone1 = new ArrayList<>();
         phone1.add("3-3-3");
-        User user1 = new User();
-//Когда пытаюсь внести данные при создании объекта - User user1 = new User(1, "Nikolay", 23, phone1);
-//почему-то выходит ошибка и не могу понять в чем проблема
-//java: constructor User in class User cannot be applied to given types;
-//required: no arguments
-//found: int,java.lang.String,int,java.util.ArrayList<java.lang.String>
-//reason: actual and formal argument lists differ in length
-        user1.fillingFields(1, "Nikolay", 23, phone1);
+        User user1 = new User(1, "Nikolay", 23, phone1);
         System.out.println("ID: " + user1.id + ";");
         System.out.println("Name: " + user1.name + ";");
         System.out.println("Age: " + user1.age + ";");
-        System.out.println("Phone: " + phone1 + ";");
+        System.out.println("Phone: " + phone1 + ".");
 
         ArrayList<String> phone2 = new ArrayList<>();
         phone2.add("3-2-8");
         phone2.add("5-4-5-5");
         phone2.add("6-7-5-4-1");
-        User user2 = new User();
-        user2.fillingFields(2, "Petr", 45, phone2);
+        User user2 = new User(2, "Petr", 45, phone2);
         System.out.println("ID: " + user2.id + ";");
         System.out.println("Name: " + user2.name + ";");
         System.out.println("Age: " + user2.age + ";");
-        System.out.println("Phone: " + phone2 + ";");
+        System.out.println("Phone: " + phone2 + ".");
 
     }
 }
